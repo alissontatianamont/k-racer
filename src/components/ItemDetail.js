13bfd18bed718de94789d2b046f23f2c79069e59
alissonM <alison_26122@hotmail.com>
2021-12-06 17:55:32 -0500

    desafio ItemDetailContainer

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,13 +1,15 @@
 import "./nav.css"
 const ItemDetail = ({product}) => {
     return(
-    <div className="tarjetas">
-       <div>
-        <h2>{product.name}</h2>
-        <img className="imagen"src={product.img} alt={product.name}/>
-        <p>Precio: {product.price} </p>
-        <button>Ver detalles</button>
-       </div>
+    <div >
+     {product?.length !==0?(
+           <div className="tarjetas">
+           <h2>{product.name}</h2>
+           <img className="imagen"src={product.img} alt={product.name}/>
+           <p>Precio: {product.price} </p>
+            <p>ACERCA DE: {product.info}</p>
+          </div>
+     ):(<div>cargando...</div>)}
     </div>)
 };
 
--- src/components/products.js
+++ src/components/products.js
@@ -1,6 +1,6 @@
 const products =[
     {id: 1, name: "Moscu", price: "$800.000", img: "https://en3dias.net/wp-content/uploads/2019/06/cuantos-dias-para-ver-moscu.jpg"},
-    {id: 2, name: "Tokio", price: "$700.000", img: "https://porlavision.com/wp-content/uploads/2020/05/Visita-a-Tokio-desde-la-casa.png"},
+    {id: 2, name: "Tokio", price: "$700.000", img: "https://porlavision.com/wp-content/uploads/2020/05/Visita-a-Tokio-desde-la-casa.png",info:"Una de las ciudades mas sorprendentes del mundo, a la vanguardia en moday en la mas sorprendente tecnologia"},
     {id: 3, name: "Berlin", price: "980.000", img: "https://www.losviajesdegrimes.com/wp-content/uploads/2020/02/catedral-berlin.jpg"},
     {id: 4, name: "Ámsterdam", price: "$950.000", img: "https://cdn-images.welcometothejungle.com/Fl88V8bG5oMNutOwPco_mIGV3t4iwSi6PgTqz4-_Mn4/rs:auto:1500::/q:85/czM6Ly93dHRqLXByb2R1Y3Rpb24vdXBsb2Fkcy9hcnRpY2xlL2ltYWdlLzY0MDYvMTU1MzI3L0Ftc3RlcmRhbS0yLmpwZw"}
 ]
